375da55ae540ecd02af89a7128e530aa036506cd
Shubhanshu <123206506+Shubhanshu0151@users.noreply.github.com>
2024-06-06 02:08:30 +0530

    Add files via upload

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:day_12/models/catlog.dart';
+import 'package:day_12/widgets/Item_widgets.dart';
+import 'package:day_12/widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Codepur";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 5));
+
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) => ItemWidget(
+                  item: CatalogModel.items[index],
+                ),
+              )
+            : const Center(
+                child: CircularProgressIndicator(
+                  color: Color.fromARGB(255, 14, 13, 13),
+                ),
+              ),
+      ),
+      drawer: const MyDrawer(),
+    );
+  }
+}
